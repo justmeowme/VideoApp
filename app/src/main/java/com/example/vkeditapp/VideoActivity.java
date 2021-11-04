96851409e081b667aed4c3466f200bf9ebede010
wis4nya <wishnya23@gmail.com>
2021-11-05 02:50:11 +0300

    Final 10

--- app/src/main/java/com/example/vkeditapp/VideoActivity.java
+++ app/src/main/java/com/example/vkeditapp/VideoActivity.java
@@ -1,15 +1,24 @@
 package com.example.vkeditapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
+
+import java.io.File;
 
 public class VideoActivity extends AppCompatActivity {
 
     VideoView mVideoView;
+    TextView mSave;
+    int WRITE_REQUEST_CODE = 1, SELECT_VIDEO_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +34,51 @@ public class VideoActivity extends AppCompatActivity {
         mVideoView.start();
 
 
+        mSave = findViewById(R.id.save);
+        mSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File file = new File(getCacheDir(), System.currentTimeMillis() + ".mp4");
+                createFile("VID-" + System.currentTimeMillis() / 1000);
+            }
+        });
     }
 
+    public void createFile(String fileName) {
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("video/mp4");
+        intent.putExtra(Intent.EXTRA_TITLE, fileName);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION | Intent.FLAG_GRANT_PREFIX_URI_PERMISSION);
+        startActivityForResult(intent, WRITE_REQUEST_CODE);
+    }
 
+    public void selectVideoFromGallery() {
+        Intent intent;
+        if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)) {
+            intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+        } else{
+            intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Video.Media.INTERNAL_CONTENT_URI);
+        }
 
+        intent.setType("video/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, SELECT_VIDEO_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_VIDEO_REQUEST && resultCode == RESULT_OK) {
+            if (data.getData() != null) {
+                Uri videoUri = data.getData();
+                String selectedVideoPath = String.valueOf(videoUri);
+                MainActivity.setDefaults("video_path", selectedVideoPath, VideoActivity.this);
+            } else {
+                Toast.makeText(getApplicationContext(), "Не удалось загрузить видео", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 
 }
